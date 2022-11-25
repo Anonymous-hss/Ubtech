507e4f5b4a6dd40d12921f223840eb69c0af8f79
Anonymous-hss <72376618+Anonymous-hss@users.noreply.github.com>
2022-11-25 13:03:31 +0530

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,22 @@
+//var main_content = document.getElementById('main_content');
+//main_content.setAttribute('align','left');
+
+//var heading = document.getElementById('heading');
+//heading.innerHTML = 'Welcome to HSS\'s Website';
+
+//var main_content = document.getElementById('main_content');
+
+//var new_heading = document.createElement('h2');
+//var new_phrase = document.createElement('p');
+
+//new_heading.innerHTML = "Welcome to HSS\'s code";
+//new_phrase.innerHTML = "This is a simple statment to show how to create a element.";
+
+//main_content.appendChild(new_heading);
+//main_content.appendChild(new_phrase);
+
+var btn = document.getElementById('mybtn');
+
+btn.onclick = function(){
+    alert('Are you sure you clicked the button ?');
+};
